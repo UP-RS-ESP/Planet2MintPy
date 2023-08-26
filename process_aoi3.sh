e1429d6aaf26811101b86e26c058a3519b890426
Bodo Bookhagen <bodo.bookhagen@uni-potsdam.de>
2023-08-26 19:45:37 +0200

    Aug-26 2023

--- process_aoi3.sh
+++ process_aoi3.sh
@@ -5,7 +5,7 @@
 # cd PS2_aoi3
 # mkdir disparity_maps
 # rsync -avz bodo@manaslu:/raid-manaslu/amueting/PhD/Project3/PlanetScope_Data/aoi3/group[1-3]/disparity_maps/*_polyfit-F.tif disparity_maps
-
+mkdir /home/bodo/Dropbox/Argentina/aoi3/
 export PYTHONPATH=$PYTHONPATH:/home/bodo/Dropbox/Argentina/Planet2mintpy
 python /home/bodo/Dropbox/Argentina/Planet2mintpy/create_offset_confidence.py \
            --kernel_size 9 \
@@ -22,29 +22,28 @@ python /home/bodo/Dropbox/Argentina/Planet2mintpy/prep_Planet_metadata.py \
   --metadata_fn PS2_aoi3_metadata.txt --processor PS2
 
 mkdir mintpy
-sed -e 's#METAFILE_FN#/raid/PS2_aoi3/PS2_aoi3_metadata.txt#' \
-  -e 's#azOffFile_FN#/raid/PS2_aoi3/disparity_maps/*-F_EW.vrt#' \
-  -e 's#rgOffFile_FN#/raid/PS2_aoi3/disparity_maps/*-F_NS.vrt#' \
-  -e 's#azOffStdFile_FN#/raid/PS2_aoi3/confidence/*_confidence.tif#' \
-  -e 's#rgOffStdFile_FN#/raid/PS2_aoi3/confidence/*_confidence.tif#' \
+sed -e 's#METAFILE_FN#/raid/Planet_NWArg/PS2_aoi3/PS2_aoi3_metadata.txt#' \
+  -e 's#azOffFile_FN#/raid/Planet_NWArg/PS2_aoi3/disparity_maps/*-F_EW.vrt#' \
+  -e 's#rgOffFile_FN#/raid/Planet_NWArg/PS2_aoi3/disparity_maps/*-F_NS.vrt#' \
+  -e 's#azOffStdFile_FN#/raid/Planet_NWArg/PS2_aoi3/confidence/*_confidence.tif#' \
+  -e 's#rgOffStdFile_FN#/raid/Planet_NWArg/PS2_aoi3/confidence/*_confidence.tif#' \
   /home/bodo/Dropbox/Argentina/Planet2mintpy/PS2_Mintpy_template.cfg \
-  >/raid/PS2_aoi3/mintpy/PS2_aoi3_config.cfg
+  >/raid/Planet_NWArg/PS2_aoi3/mintpy/PS2_aoi3_config.cfg
 
 python /home/bodo/Dropbox/Argentina/Planet2mintpy/prep_Planet_stack.py \
-          --dx_fn "/raid/PS2_aoi3/disparity_maps/*-F_EW.vrt" \
-          --dy_fn "/raid/PS2_aoi3/disparity_maps/*-F_NS.vrt" \
-          --dx_confidence_fn "/raid/PS2_aoi3/confidence/*_confidence.tif" \
-          --dy_confidence_fn "/raid/PS2_aoi3/confidence/*_confidence.tif" \
-          --meta_file /raid/PS2_aoi3/PS2_aoi3_metadata.txt --pixel_size 3.0 \
-          --template_file /raid/PS2_aoi3/mintpy/PS2_aoi3_config.cfg \
-          --h5_stack_fn /raid/PS2_aoi3/mintpy/inputs/geo_offsetStack_aoi3.h5
+          --dx_fn "/raid/Planet_NWArg/PS2_aoi3/disparity_maps/*-F_EW.vrt" \
+          --dy_fn "/raid/Planet_NWArg/PS2_aoi3/disparity_maps/*-F_NS.vrt" \
+          --dx_confidence_fn "/raid/Planet_NWArg/PS2_aoi3/confidence/*_confidence.tif" \
+          --dy_confidence_fn "/raid/Planet_NWArg/PS2_aoi3/confidence/*_confidence.tif" \
+          --meta_file /raid/Planet_NWArg/PS2_aoi3/PS2_aoi3_metadata.txt --pixel_size 3.0 \
+          --template_file /raid/Planet_NWArg/PS2_aoi3/mintpy/PS2_aoi3_config.cfg \
+          --h5_stack_fn /raid/Planet_NWArg/PS2_aoi3/mintpy/inputs/geo_offsetStack_aoi3.h5
 
 cd raid/PS2_aoi3/mintpy/
-ifgram_inversion.py /raid/PS2_aoi3/mintpy/inputs/geo_offsetStack_aoi3.h5 \
+ifgram_inversion.py /raid/Planet_NWArg/PS2_aoi3/mintpy/inputs/geo_offsetStack_aoi3.h5 \
     --skip-reference -i azimuthOffset -w var -c local --num-worker 20 --mem 16 -o timeseriesAz_var.h5 residualInvAz_var.h5 numInvOffsetAz_var.h5
-ifgram_inversion.py /raid/PS2_aoi3/mintpy/inputs/geo_offsetStack_aoi3.h5 \
+ifgram_inversion.py /raid/Planet_NWArg/PS2_aoi3/mintpy/inputs/geo_offsetStack_aoi3.h5 \
     --skip-reference -i rangeOffset -w var -c local --num-worker 20 --mem 16 -o timeseriesRg_var.h5 residualInvRg_var.h5 numInvOffsetRg_var.h5
-```
 
 view.py timeseriesAz_var.h5 --nodisplay --vlim -2 2 -u m --title "PS2 var: Az (NS) timeseries" -c RdYlBu --save -o PS2_ts_NS_var.png
 view.py timeseriesRg_var.h5 --nodisplay --vlim -2 2 -u m --title "PS2 var: Rg (EW) timeseries" -c RdYlBu --save -o PS2_ts_EW_var.png
@@ -65,9 +64,9 @@ convert -quality 50 PS2_EW_var_nomask_meanv.png /home/bodo/Dropbox/Argentina/aoi
 convert -quality 50 PS2_EW_var_nomask_stats.png /home/bodo/Dropbox/Argentina/aoi3/PS2_aoi3_EW_var_nomask_stats.jpg
 
 python /home/bodo/Dropbox/Argentina/Planet2mintpy/calc_velocity_from_AzRg.py \
-    --rg_file /raid/PS2_aoi3/mintpy/velocityRg_var.h5 \
-    --az_file /raid/PS2_aoi3/mintpy/velocityAz_var.h5 \
-    --HDF_outfile /raid/PS2_aoi3/mintpy/aoi3_var_velocity.h5 \
+    --rg_file /raid/Planet_NWArg/PS2_aoi3/mintpy/velocityRg_var.h5 \
+    --az_file /raid/Planet_NWArg/PS2_aoi3/mintpy/velocityAz_var.h5 \
+    --HDF_outfile /raid/Planet_NWArg/PS2_aoi3/mintpy/aoi3_var_velocity.h5 \
     --mask00 False
 
 view.py aoi3_var_velocity.h5 vel_magnitude -u meters --vlim 0 4 --nodisplay \
@@ -82,9 +81,9 @@ view.py aoi3_var_velocity.h5 vel_direction --vlim 0 360 -c hsv --nodisplay \
 # view.py aoi3_var_velocity.h5 vel_magnitude --mask aoi3_var_velocity_mask.h5 -u meters --vlim 3 6 --nodisplay --title "PS2 var: Velocity Magnitude" --zero-mask --save -o PS2_var_vel_magnitude_mask.png
 # view.py aoi3_var_velocity.h5 vel_direction --mask aoi3_var_velocity_mask.h5 --vlim 0 360 -c hsv --nodisplay --title "PS2 var: Velocity Direction" --save -o PS2_var_vel_direction_mask.png
 # extract_mask_TS.py \
-#     --az_ts_file /raid/PS2_aoi3/mintpy/timeseriesAz_var.h5 \
-#     --rg_ts_file /raid/PS2_aoi3/mintpy/timeseriesRg_var.h5 \
-#     --mask_file /raid/PS2_aoi3/mintpy/aoi3_var_velocity_mask.h5 \
+#     --az_ts_file /raid/Planet_NWArg/PS2_aoi3/mintpy/timeseriesAz_var.h5 \
+#     --rg_ts_file /raid/Planet_NWArg/PS2_aoi3/mintpy/timeseriesRg_var.h5 \
+#     --mask_file /raid/Planet_NWArg/PS2_aoi3/mintpy/aoi3_var_velocity_mask.h5 \
 #     --HDF_outfile  /raid/PS2_aoi6/mintpy/aoi3_var_velocity_mask_ts.h5 \
 #     --npy_outfile /raid/PS2_aoi6/mintpy/aoi3_var_velocity_mask_ts.npy \
 #     --out_pngfname /raid/PS2_aoi6/mintpy/aoi3_var_velocity_mask_ts.png
